cd2be6dc7c628e9227fc2b234eaa5859175f33f5
Kocsis Viktor <achilleus88888@gmail.com>
2016-05-04 15:14:10 +0200

    add serialize and deserialize method

--- Serializer/Serializer/Person.cs
+++ Serializer/Serializer/Person.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +21,10 @@ namespace Serializer
         [NonSerialized]
         private string SerialNumber;
 
+        public Person()
+        {
+
+        }
         public Person(string name, string address, string phone, DateTime dateOfData)
         {
             Name = name;
@@ -27,6 +33,24 @@ namespace Serializer
             DateOfData = dateOfData;
         }
 
+        public void Serialize(string fileName)
+        {
+            FileStream fileStream = new FileStream(fileName, FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            DateOfData = DateTime.Now;
+            formatter.Serialize(fileStream, this);
+            fileStream.Close();
+        }
+
+        public static Person Deserialize(string fileName)
+        {
+            FileStream fileStream = new FileStream(fileName, FileMode.Open);
+            BinaryFormatter formatter = new BinaryFormatter();
+            var person = (Person)formatter.Deserialize(fileStream);
+            fileStream.Close();
+
+            return person;
+        }
         public void OnDeserialization(object sender)
         {
             throw new NotImplementedException();
